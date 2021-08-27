c3f8e59d57fd962a576ebeced9895764973668ff
LucasAccurcio <lucas.accurcio@gmail.com>
2021-08-26 21:59:27 -0300

    Jogo funcionando 100%

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const textRGB = document.getElementById('rgb-color');
 const arrayBalls = document.querySelectorAll('.ball');
 const answer = document.getElementById('answer');
+const btnReset = document.getElementById('reset-game');
+const showScore = document.getElementById('score');
+let score = 0;
 
 function randomColor() {
   const color1 = Math.floor(Math.random() * 256);
@@ -8,13 +11,39 @@ function randomColor() {
   const color3 = Math.floor(Math.random() * 256);
   return `(${color1}, ${color2}, ${color3})`;
 }
-const colorGuess = randomColor();
-textRGB.innerText = colorGuess;
-const theRigthBall = Math.floor(Math.random() * 6);
-console.log(`Posição no array da bola correta: ${theRigthBall}`);
-console.log(`Cor da bola correta ${colorGuess}`);
 
-function balls(callback) {
+function newColorGuess() {
+  const colorGuess = randomColor();
+  textRGB.innerText = colorGuess;
+  return colorGuess;
+}
+
+function randomBall() {
+  const theRigthBall = Math.floor(Math.random() * 6);
+  return theRigthBall;
+}
+
+function addListener(theRigthBall) {
+  let click = 0;
+  arrayBalls.forEach((element, index) => element.addEventListener('click', () => {
+    if (click === 1) {
+      return console.log('Você já jogou!');
+    }
+    if (index === theRigthBall) {
+      answer.innerText = 'Acertou!';
+      score += 3;
+      showScore.innerText = score;
+      arrayBalls[index].classList.add('selected');
+      click = 1;
+    } else {
+      answer.innerText = 'Errou! Tente novamente!';
+      arrayBalls[index].classList.add('selected');
+      click = 1;
+    }
+  }));
+}
+
+function balls(callback, colorGuess, theRigthBall) {
   for (let i = 0; i < arrayBalls.length; i += 1) {
     if (i === theRigthBall) {
       arrayBalls[i].style.backgroundColor = `rgb${colorGuess}`;
@@ -22,13 +51,22 @@ function balls(callback) {
       arrayBalls[i].style.backgroundColor = `rgb${callback()}`;
     }
   }
+  addListener(theRigthBall);
 }
-balls(randomColor);
 
-arrayBalls.forEach((element, index) => element.addEventListener('click', () => {
-  if (index === theRigthBall) {
-    answer.innerText = 'Acertou!';
-  } else {
-    answer.innerText = 'Errou! Tente novamente!';
-  }
-}));
+function startGame() {
+  const selected = document.querySelector('.selected');
+  if (selected !== null) selected.classList.remove('selected');
+  answer.innerText = 'Escolha uma cor';
+  const colorGuess = newColorGuess();
+  const theRigthBall = randomBall();
+  balls(randomColor, colorGuess, theRigthBall);
+}
+
+btnReset.addEventListener('click', () => {
+  startGame();
+});
+
+window.onload = () => {
+  startGame();
+};
